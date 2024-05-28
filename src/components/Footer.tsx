56289abba6c907dda120ff679f619af042f44094
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-28 22:42:20 +0100

    Created Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h3>Coded by 
+            <a
+             href='https://www.lunasmithart.com/' 
+             target='_blank'
+             className='portfolio'>
+                Luna Smith
+            </a>
+            -
+            <a 
+            href='https://github.com/Lu-Smith/poetry-react'
+            target='_blank'>
+                GitHub
+            </a>
+            .
+        </h3>
+    </div>
+  )
+}
+
+export default Footer
