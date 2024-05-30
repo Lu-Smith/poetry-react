297ccc4a439c0c735ef242ae6279f961cc7ad289
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-30 22:08:08 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  background-color: black;
+  color: pink;
+`;
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <HeaderContainer>Header</HeaderContainer>
   )
 }
 
