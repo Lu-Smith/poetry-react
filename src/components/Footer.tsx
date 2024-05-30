aed303f02fa3d93cf3fd160bb3dc8ecadc2eef29
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-30 22:06:46 +0100

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+  background-color: black;
+  color: pink;
+`;
+
+const StyledLink = styled.a`
+  color: red;
+`;
 
 const Footer = () => {
   return (
-    <div>
-        <h3>Coded by 
-            <a
-             href='https://www.lunasmithart.com/' 
-             target='_blank'
-             className='portfolio'>
-                Luna Smith
-            </a>
-            -
-            <a 
-            href='https://github.com/Lu-Smith/poetry-react'
-            target='_blank'>
-                GitHub
-            </a>
-            .
-        </h3>
-    </div>
+    <FooterContainer>
+      <h3>
+        Coded by{' '}
+        <StyledLink
+          href='https://www.lunasmithart.com/'
+          target='_blank'
+          className='portfolio'
+        >
+          Luna Smith
+        </StyledLink>
+        -{' '}
+        <StyledLink
+          href='https://github.com/Lu-Smith/poetry-react'
+          target='_blank'
+        >
+          GitHub
+        </StyledLink>
+        .
+      </h3>
+    </FooterContainer>
   )
 }
 
