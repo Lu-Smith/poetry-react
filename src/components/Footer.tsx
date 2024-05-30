5a6a1425994dd4d87417906655ffb7a778efa27d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-30 22:17:13 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,24 @@ import styled from 'styled-components';
 const FooterContainer = styled.div`
   background-color: black;
   color: pink;
+  padding: 20px;
+  border-top: 2px solid pink;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Arial', sans-serif;
+  font-size: 16px;
+  text-align: center;
 `;
 
 const StyledLink = styled.a`
   color: red;
+  text-decoration: none;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: lightcoral;
+  }
 `;
 
 const Footer = () => {
@@ -18,11 +32,10 @@ const Footer = () => {
         <StyledLink
           href='https://www.lunasmithart.com/'
           target='_blank'
-          className='portfolio'
         >
           Luna Smith
         </StyledLink>
-        -{' '}
+        {' '}-{' '}
         <StyledLink
           href='https://github.com/Lu-Smith/poetry-react'
           target='_blank'
