b118c8cd9e5ab94f22e2c047d95b34cef0a6b14d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-31 13:30:50 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import Footer from './components/Footer'
-import Header from './components/Header'
-import IntroPage from './components/IntroPage'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import IntroPage from './components/IntroPage';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  background-color: #feffdf;
+  color: #5b446a;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+  height: 100vh;
+  width: 100vw;
+`;
 
 function App() {
   return (
-    <>
+    <MainContainer>
       <Header />
       <IntroPage />
       <Footer />
-    </>
+    </MainContainer>
   )
 }
 
