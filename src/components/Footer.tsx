0fdc4c68a7c43929d68360ee30c42eb8f69f21f1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-31 13:31:02 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,25 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterContainer = styled.div`
-  background-color: black;
-  color: pink;
+  background-color: #906387;
+  color: #7dd87d;
   padding: 20px;
-  border-top: 2px solid pink;
+  border-top: 2px solid #7dd87d;
   display: flex;
   justify-content: center;
   align-items: center;
   font-family: 'Arial', sans-serif;
   font-size: 16px;
   text-align: center;
+  width: 100vw;
 `;
 
 const StyledLink = styled.a`
-  color: red;
+  color: #feffdf;
   text-decoration: none;
   transition: color 0.3s ease;
 
   &:hover {
-    color: lightcoral;
+    color: #7dd87d;
   }
 `;
 
