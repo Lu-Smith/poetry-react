e48cf86d1d8932eceb2e35ac9e06aa3fcc51e94f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-04 22:28:19 +0100

    Updated styling in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,16 +29,16 @@ const ModeContainer = styled.div`
 `;
 
 const ModeButton = styled.button`
-  background-color: #7dd87d;
-  color: #283739;
+  background-color: #4c9173;
+  color: #feffdf;
   cursor: pointer;
   padding: 5px 10px;
   border-radius: 5px;
   transition: all 0.3s ease;
 
   &:hover {
-    color: #feffdf;
-    background-color: #4c9173;
+    color: #283739;
+    background-color: #7dd87d;
   }
 `;
 
