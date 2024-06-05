f7e6a07aa39cbedaf4febe74856a08931acfe830
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-05 23:10:38 +0100

    Updated IntroPage.tsx

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -1,10 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Particle from '../assets/Particle';
-
-interface Mouse {
-  x: number | undefined;
-  y: number | undefined;
-}
+import Particle, { Mouse } from '../assets/Particle';
 
 const IntroPage: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -30,17 +25,19 @@ const IntroPage: React.FC = () => {
     const handleClick = (event: MouseEvent) => {
       mouse.current.x = event.x;
       mouse.current.y = event.y;
-      for (let i = 0; i < 10; i++) {
-        setParticlesArray((prev) => [...prev, new Particle(mouse.current, hue)]);
-      }
+      setParticlesArray((prev) => [
+        ...prev,
+        new Particle(mouse.current, hue),
+      ]);
     };
 
     const handleMouseMove = (event: MouseEvent) => {
       mouse.current.x = event.x;
       mouse.current.y = event.y;
-      for (let i = 0; i < 10; i++) {
-        setParticlesArray((prev) => [...prev, new Particle(mouse.current, hue)]);
-      }
+      setParticlesArray((prev) => [
+        ...prev,
+        new Particle(mouse.current, hue),
+      ]);
     };
 
     canvas.addEventListener('click', handleClick);
@@ -52,7 +49,7 @@ const IntroPage: React.FC = () => {
       canvas.removeEventListener('mousemove', handleMouseMove);
     };
     
-  }, []);
+  }, [hue]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -61,42 +58,19 @@ const IntroPage: React.FC = () => {
 
     const animate = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      handleParticles(ctx);
-      setHue((prevHue) => prevHue + 2);
+      particlesArray.forEach((particle) => {
+        particle.update();
+        particle.draw(ctx);
+      });
+
+      setHue((prevHue) => prevHue + 1);
       requestAnimationFrame(animate);
     };
 
     animate();
   }, [particlesArray]);
 
-  const handleParticles = (ctx: CanvasRenderingContext2D) => {
-    particlesArray.forEach((particle, index) => {
-      particle.update();
-      particle.draw(ctx);
-
-      for (let j = index; j < particlesArray.length; j++) {
-        const dx = particle.x - particlesArray[j].x;
-        const dy = particle.y - particlesArray[j].y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-        if (distance < 100) {
-          ctx.beginPath();
-          ctx.strokeStyle = particle.color;
-          ctx.lineWidth = 0.2;
-          ctx.moveTo(particle.x, particle.y);
-          ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
-          ctx.stroke();
-          ctx.closePath();
-        }
-      }
-
-      if (particle.size <= 0.3) {
-        setParticlesArray((prev) => prev.filter((_, i) => i !== index));
-      }
-    });
-  };
-
-
-
+ 
   return (
     <>
       <canvas ref={canvasRef}  id="canvas1"></canvas>
