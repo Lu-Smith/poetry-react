2e1560051d44dc0a1df684c8d1437bbb36c30a4b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-05 22:17:36 +0100

    Updated IntroPage.tsx

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react';
 
 const IntroPage = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
   return (
-    <h2>Welcome to the <span>💗</span> of <span>Scottish Poetry</span> - welcome home.</h2>
+    <>
+      <canvas ref={canvasRef}  id="canvas1"></canvas>
+      <h2>Welcome to the <span>💗</span> of <span>Scottish Poetry</span> - welcome home.</h2>
+    </>
   )
 }
 
