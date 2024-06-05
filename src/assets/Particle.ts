663c2abe76b6293cd803c3f02d5eedbf70cc63aa
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-05 22:27:53 +0100

    Updated Particle.ts

--- src/assets/Particle.ts
+++ src/assets/Particle.ts
@@ -4,4 +4,10 @@ export default class Particle {
     x: number;
     y: number;
     effect: Effect;
+
+    constructor(effect: Effect) {
+        this.effect = effect;
+        this.x = Math.floor(Math.random() * this.effect.width);
+        this.y = Math.floor(Math.random() * this.effect.height);
+    }
 }
