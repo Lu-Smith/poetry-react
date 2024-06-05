b47816e427b5db900681c8a73de6b1a8dc989a89
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-05 22:38:08 +0100

    Updated Particle.ts

--- src/assets/Particle.ts
+++ src/assets/Particle.ts
@@ -1,13 +1,23 @@
-import Effect from "./Effect";
-
 export default class Particle {
     x: number;
     y: number;
-    effect: Effect;
+    size: number;
+    speedX: number;
+    speedY: number;
+    color: string;
 
-    constructor(effect: Effect) {
-        this.effect = effect;
-        this.x = Math.floor(Math.random() * this.effect.width);
-        this.y = Math.floor(Math.random() * this.effect.height);
+    constructor(mouse: Mouse, hue: number) {
+        this.x = mouse.x || 0;
+        this.y = mouse.y || 0;
+        this.size = Math.random() * 20 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = `hsl(${hue}, 100%, 50%)`;
+    }
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
     }
 }
