0cfb242c959786074144202142c69575d94131a9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-07 19:49:24 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
+interface Props {
+  mode: boolean,
+  toggleMode: () => void
+}
+
 const HeaderContainer = styled.div`
   background-color: #53a8b6;
   display: flex;
@@ -11,14 +16,26 @@ const HeaderContainer = styled.div`
   padding: 10px;
   width: 100vw;
   z-index: 99;
+
+  &.light {
+    background-color: #ffffff;
+  }
 `;
 
 const Title = styled.h1`
   color: #484c4d;
-  transition: all 0.3s ease;
+  transition: color 0.3s ease;
+
+  &.light {
+    color: rgb(181, 86, 86);
+  }
 
   &:hover {
     color: #ffffff;
+  }
+
+  &.light: hover {
+    color: #5585b5;
   }
 `;
 
@@ -27,6 +44,7 @@ const ModeContainer = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+  gap: 3px
 `;
 
 const ModeButton = styled.button`
@@ -37,29 +55,47 @@ const ModeButton = styled.button`
   border-radius: 5px;
   transition: all 0.3s ease;
 
+  &.light {
+  background-color: #ffffff;
+  border: 2px solid rgb(181, 86, 86);
+}
+
   &:hover {
-    background-color: #79c2d0;
+    background-color: #F3AD61;
+  }
+
+  .light &:hover {
+  background-color: #ffffff;
+  border: 2px solid rgb(235, 196, 196);
+  }
+
+  @media screen and (max-width: 786px) {
+    padding: 5px 8px;
+    border: 1px solid #79c2d0;
+    font-size: 0.3rem;
   }
 `;
 
 
-const Header: React.FC = () => {
+const Header: React.FC<Props> = ({mode, toggleMode}) => {
   return (
-    <HeaderContainer>
-      <Title>
+    <HeaderContainer className={mode ? "light" : ""}>
+      <Title className={mode ? "light" : ""}>
         Scottish Poetry
       </Title>
-      <ModeContainer>
-        <ModeButton>
+      <ModeContainer className={mode ? "light" : ""}>
+        {mode ? 
+        <ModeButton onClick={toggleMode} className={mode ? "light" : ""}>
           <span className="material-symbols-outlined">
             emoji_objects
           </span>
-        </ModeButton>
-        <ModeButton>
+        </ModeButton>:
+        <ModeButton onClick={toggleMode} className={mode ? "light" : ""} >
           <span className="material-symbols-outlined">
             nightlight
           </span>
         </ModeButton>
+        }
       </ModeContainer>
     </HeaderContainer>
   )
