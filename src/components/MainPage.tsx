4c99ee133511a4d74ca799275140eca9c8d3f2ed
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-08 23:46:40 +0100

    Updated MainPage.tsx

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { ButtonStyle } from '../styles/Button';
 import Poem from './Poem';
 import poems from '../assets/poems';
+import styled from 'styled-components';
 
 interface Props {
     mode: boolean,
     toggleHome: () => void,
 }
 
+const PoemsContainer = styled.div`
+
+`;
+
 const MainPage: React.FC<Props> = ({mode, toggleHome}) => {
   return (
-    <>
+    <PoemsContainer>
       <ButtonStyle className={mode ? "light home" : "home"} onClick={toggleHome}>
         <span className="material-symbols-outlined">
           home
@@ -19,7 +24,7 @@ const MainPage: React.FC<Props> = ({mode, toggleHome}) => {
         {Object.keys(poems).map(poemKey => (
           <Poem key={poemKey} mode={mode} poem={poems[poemKey]} />
         ))}
-    </>
+    </PoemsContainer>
   )
 }
 
