3b4cf986763a88b030541c6f3e3b3dff1baddc95
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-09 15:22:23 +0100

    Updated MainPage.tsx

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -3,6 +3,7 @@ import { ButtonStyle } from '../styles/Button';
 import Poem from './Poem';
 import poems from '../assets/poems';
 import styled from 'styled-components';
+import { motion } from "framer-motion";
 
 interface Props {
     mode: boolean,
@@ -10,21 +11,43 @@ interface Props {
 }
 
 const PoemsContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: start;
+    gap: 40px;
+    overflow: scroll;
+    margin-bottom: 100px;
 
+    div {
+      width: 60%;
+      margin: 0 auto;
+    }
 `;
 
 const MainPage: React.FC<Props> = ({mode, toggleHome}) => {
   return (
-    <PoemsContainer>
+    <>
       <ButtonStyle className={mode ? "light home" : "home"} onClick={toggleHome}>
         <span className="material-symbols-outlined">
           home
         </span>
       </ButtonStyle>
+      <PoemsContainer>
         {Object.keys(poems).map(poemKey => (
-          <Poem key={poemKey} mode={mode} poem={poems[poemKey]} />
-        ))}
-    </PoemsContainer>
+          <motion.div>
+            <Poem key={poemKey} mode={mode} poem={poems[poemKey]} />
+          </motion.div>
+          ))}
+      </PoemsContainer>
+      <ButtonStyle className={mode ? "light scroll" : "scroll"} onClick={toggleHome}>
+        <span className="material-symbols-outlined">
+        stat_minus_2
+        </span>
+      </ButtonStyle>
+    
+    </>
   )
 }
 
