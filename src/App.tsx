b995950a449863d1e04d9582cd08db613debecdb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-10 17:29:53 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,35 @@ import styled from 'styled-components';
 
 const MainContainer = styled.div`
   color: #5b446a;
-  height: 100vh;
+  height: 100%;
   width: 100vw;
-  background-color: #2c3031;
+    background-color: #2c3031;
 
   &.light {
-    background-color: rgb(235, 196, 196);
+    background-color: rgb(245, 223, 223);
   }
 `;
 
 function App() {
   const [mode, setMode] = useState(false);
+  const [poems, setPoems] = useState(false);
 
   const toggleMode = () => {
     setMode(prev => !prev);
   };
 
+  const toggleHome = () => {
+    setPoems(prev => !prev);
+  };
+
+  const goHome = () => {
+    setPoems(false);
+  };
+
   return (
     <MainContainer className={mode ? "light" : ""}>
-      <Header mode = {mode} toggleMode = {toggleMode} />
-      <IntroPage mode = {mode} />
+      <Header mode = {mode} toggleMode = {toggleMode} goHome = {goHome} />
+      <IntroPage mode = {mode} toggleHome = {toggleHome} poems = {poems} />
       <Footer mode = {mode} />
     </MainContainer>
   )
