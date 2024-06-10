7f957584e4d1d0cd871b40d19768bb5e39c6c0a8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-06-10 14:04:29 +0100

    Updated IntroPage.tsx

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import Particle, { Mouse } from '../assets/Particle';
 import MainPage from './MainPage';
 import Intro from './Intro';
@@ -9,13 +10,48 @@ interface Props {
 }
 
 const IntroPageContainer = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: start;
-    gap: 20px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  gap: 20px;
+
+  .mainContainer {
+    z-index: 99;
+    text-align: center;
+    overflow-x: hidden;
+    margin-top: 40px;
+
+    button {
+    margin-top: 40px;
+    }
+  }
 `;
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    x: '100vw',
+    y: '-100vh',
+  },
+  in: {
+    opacity: 1,
+    x: 0,
+    y: 0,
+  },
+  out: {
+    opacity: 0,
+    x: '-100vw',
+    y: '100vh',
+  },
+};
+
+const pageTransition = {
+  type: 'spring',
+  stiffness: 40,
+  damping: 12,
+};
 
 const IntroPage: React.FC<Props> = ({mode}) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -27,32 +63,6 @@ const IntroPage: React.FC<Props> = ({mode}) => {
   const toggleHome = () => {
     setPoems(prev => !prev);
   };
-
-  function handleParticle(ctx: CanvasRenderingContext2D) {
-    for ( let i = 0; i < particlesArray.length; i++) {
-        particlesArray[i].update();
-        particlesArray[i].draw(ctx);
-        for ( let j = i; j < particlesArray.length; j++) {
-            const dx = particlesArray[i].x - particlesArray[j].x;
-            const dy = particlesArray[i].y - particlesArray[j].y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-            if ( distance < 200) {
-                ctx.beginPath();
-                ctx.strokeStyle = particlesArray[i].color;
-                ctx.lineWidth = 1;
-                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
-                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
-                ctx.stroke();
-                ctx.closePath();
-            }
-        }
-        if (particlesArray[i].size <= 0.3) {
-            particlesArray.splice(i, 1);
-            i--;
-        }
-    }
-}
-
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -106,6 +116,31 @@ const IntroPage: React.FC<Props> = ({mode}) => {
     const ctx = canvas?.getContext('2d');
     if (!canvas || !ctx) return;
 
+    function handleParticle(ctx: CanvasRenderingContext2D) {
+      for ( let i = 0; i < particlesArray.length; i++) {
+          particlesArray[i].update();
+          particlesArray[i].draw(ctx);
+          for ( let j = i; j < particlesArray.length; j++) {
+              const dx = particlesArray[i].x - particlesArray[j].x;
+              const dy = particlesArray[i].y - particlesArray[j].y;
+              const distance = Math.sqrt(dx * dx + dy * dy);
+              if ( distance < 200) {
+                  ctx.beginPath();
+                  ctx.strokeStyle = particlesArray[i].color;
+                  ctx.lineWidth = 1;
+                  ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                  ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                  ctx.stroke();
+                  ctx.closePath();
+              }
+          }
+          if (particlesArray[i].size <= 0.3) {
+              particlesArray.splice(i, 1);
+              i--;
+          }
+      }
+  }
+
     const animate = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       handleParticle(ctx);
@@ -138,7 +173,7 @@ const IntroPage: React.FC<Props> = ({mode}) => {
     };
 
     animate();
-  }, [particlesArray]);
+  }, [particlesArray, mode]);
 
   
 
@@ -146,11 +181,25 @@ const IntroPage: React.FC<Props> = ({mode}) => {
   return (
     <IntroPageContainer>
       <canvas ref={canvasRef}  id="canvas1" className={mode ? "light" : ""}></canvas>
-      {poems ? 
-        <MainPage mode = {mode} toggleHome = {toggleHome} />
-        :
-        <Intro mode = {mode} toggleHome = {toggleHome} />
-      }
+      <AnimatePresence mode="wait">
+        {poems ? (
+          <motion.div className='mainContainer'
+            key="main"
+            initial="initial"
+            animate="in"
+            exit="out"
+            variants={pageVariants}
+            transition={pageTransition}
+          >
+            <MainPage mode={mode} toggleHome={toggleHome} />
+          </motion.div>
+        ) : (
+          <div className='mainContainer'
+          >
+            <Intro mode={mode} toggleHome={toggleHome} />
+          </div>
+        )}
+      </AnimatePresence>
     </IntroPageContainer>
   )
 }
